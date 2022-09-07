d93521d8710e96dff7bdedf95d188af3d021088c
Zakariya Hassan <79968201+zakariya6002@users.noreply.github.com>
2022-09-07 13:38:27 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import Button from "@mui/material/Button";
+import ButtonIcon from "./ButtonIcon";
+
+export default function App() {
+  return (
+    <>
+      <ButtonIcon />
+    </>
+  );
+}
